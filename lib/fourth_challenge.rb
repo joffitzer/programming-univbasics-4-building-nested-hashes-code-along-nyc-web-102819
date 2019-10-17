39482196142d0413a1f3ab5ff590034c04173fb3
Jonah Offitzer <joffitzer@gmail.com>
2019-10-17 01:39:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name 
+        {name = "Benvolio", age = "17", attitude = "worried"}
+        {name = "Mercutio", age = "18", attitude = "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -13,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name = "Steven", age = "30", attitude = "confused"}
+        {name = "Nurse", age = "44", attitude = "worried"}
       ]
    }
 }
@@ -21,41 +23,3 @@ def fourth_challenge
   
 
 end
-
-The hero's two friends are Benvolio and Mercutio. So, the `:hero_friends` array
-will contain two hashes. Each of these two hashes has the following three keys:
-
-- `:name`
-- `:age`
-- `:attitude`
-
-The hero's first friend has
-
-- a name of "Benvolio",
-- an age of "17", and
-- an attitude of "worried".
-
-The hero's second friend has
-
-- a name of "Mercutio",
-- an age of "18", and
-- an attitude of "hot-headed".
-
-The heroine's two friends are Steven and Nurse. So, the `:heroine_friends` array
-will contain two hashes. Each of these two hashes has the following three keys:
-
-- `:name`
-- `:age`
-- `:attitude`
-
-The heroine's first friend has
-
-- a name of "Steven",
-- an age of "30", and
-- an attitude of "confused".
-
-The heroine's second friend has
-
-- a name of "Nurse",
-- an age of "44", and
-- an attitude of "worried".
