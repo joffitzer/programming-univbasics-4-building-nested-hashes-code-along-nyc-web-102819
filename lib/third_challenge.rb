4cf487f3db98bd2bbc45dfe3f144a33e3dfc425c
Jonah Offitzer <joffitzer@gmail.com>
2019-10-17 01:33:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -36,24 +36,3 @@ def third_challenge
 
   
 end
-
-- The Montague `:patriarch` has
-  - a `:name` of "Lord Montague" and
-  - an `:age` of "53".
-- The Montague `:matriarch` has
-  - a `:name` of "Lady Montague" and
-  - an `:age` of "54".
-- The Montague `:hero` has
-  - a `:name` of "Romeo",
-  - an `:age` of "15", and
-  - a `:status` of "alive".
-- The Capulet `:patriarch` has
-  - a `:name` of "Lord Capulet" and
-  - an `:age` of "50".
-- The Capulet `:matriarch` has
-  - a `:name` of "Lady Capulet" and
-  - an `:age` of "51".
-- The Capulet `:heroine` has
-  - a `:name` of "Juliet",
-  - an `:age` of "15", and
-  - a `:status` of "alive".
