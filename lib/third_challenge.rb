969c178be2495a4b02880224ad9afa90ddce9aaa
Jonah Offitzer <joffitzer@gmail.com>
2019-10-17 01:29:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,10 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
@@ -28,3 +30,24 @@ def third_challenge
 
   
 end
+
+- The Montague `:patriarch` has
+  - a `:name` of "Lord Montague" and
+  - an `:age` of "53".
+- The Montague `:matriarch` has
+  - a `:name` of "Lady Montague" and
+  - an `:age` of "54".
+- The Montague `:hero` has
+  - a `:name` of "Romeo",
+  - an `:age` of "15", and
+  - a `:status` of "alive".
+- The Capulet `:patriarch` has
+  - a `:name` of "Lord Capulet" and
+  - an `:age` of "50".
+- The Capulet `:matriarch` has
+  - a `:name` of "Lady Capulet" and
+  - an `:age` of "51".
+- The Capulet `:heroine` has
+  - a `:name` of "Juliet",
+  - an `:age` of "15", and
+  - a `:status` of "alive".
